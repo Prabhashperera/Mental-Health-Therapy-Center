ade070580d87acb9af89f4e783af54f36cd37e34
Prabhashperera <prabashmperera01@gmail.com>
2025-04-20 19:05:10 +0530

    INIT

--- src/main/java/lk/project/healthCareCenter/controller/HomePageController.java
+++ src/main/java/lk/project/healthCareCenter/controller/HomePageController.java
@@ -1,17 +1,23 @@
 package lk.project.healthCareCenter.controller;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class HomePageController implements Initializable {
+    public Label currentDateShow;
+    public Label currentTimeShow;
     private LoginPageController loginPageController;
     public static String userRole = null;
     @FXML
@@ -19,7 +25,38 @@ public class HomePageController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy / MM / dd");
+        String currentDate = dateFormat.format(new Date());
+        currentDateShow.setText(currentDate);
+        currentTimeShowThread();
     }
+
+    //Time & Date Methods
+    public void currentTimeShowThread() {
+        Thread thread = new Thread(() -> {
+            SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss"); // 24-hour format
+            while (true) {
+                String currentTime = timeFormat.format(new Date());
+                Platform.runLater(() -> {
+                    currentTimeShow.setText(currentTime);
+                });
+                try {
+                    Thread.sleep(1000); // Sleep for 1 second
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.setDaemon(true); // Let the app close gracefully
+        thread.start();
+    }
+    public String getCurrentDate() {
+        return currentDateShow.getText();
+    }
+    public String getCurrentTime() {
+        return currentTimeShow.getText();
+    }
+
 
     @FXML
     private void registerOnClick(ActionEvent actionEvent) throws IOException {
--- src/main/java/lk/project/healthCareCenter/entity/Payment.java
+++ src/main/java/lk/project/healthCareCenter/entity/Payment.java
@@ -12,7 +12,6 @@ import lombok.*;
 @Entity
 public class Payment {
     @Id
-    private String paymentID;
     private String paymentDate;
     private int paymentAmount;
 
